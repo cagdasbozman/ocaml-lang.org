4d8c376014b5a145585a21dec073a616d853472a
Fabrice Le Fessant <fabrice@ocamlpro.com>
2012-02-02 19:57:55 +0100

    Revert "Start using the same style as ocamlweb"
    This reverts commit 063788dc64dd1c71070a9244d368d17983059238.

---
+++ www/js/main.js
@@ -0,0 +1,29 @@
+
+function ReturnDivByClass(class_parent){
+	var DIVcoll = document.getElementsByTagName("DIV"), hiddenDivs = new Array();
+	for (LC = 0; LC < DIVcoll.length; LC ++) {
+		if (DIVcoll[LC].className==class_parent){
+			return DIVcoll[LC];
+		}
+	}
+}
+
+
+var tempBL;
+function PrevuImpression(){
+    var disp_setting="toolbar=no,location=no,directories=no,menubar=no,status=no,";
+    disp_setting+="scrollbars=yes,width=700, height=600, left=25, top=25";
+    var docprint=window.open("","PrintWindow",disp_setting);
+    tempBL = ReturnDivByClass("Content");
+    var head=document.getElementsByTagName("head")[0].innerHTML;
+    var body=document.getElementsByTagName("body")[0].innerHTML;
+    var strprint=
+	'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"\n'
+	+ '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n'
+	+ '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">'
+	+ '<head>\n' + head + '</head>\n'
+	+ '<body id="Print">\n' + body + '</body>\n'
+	+ '</html>';
+    docprint.document.write(strprint);
+    docprint.focus();
+}
